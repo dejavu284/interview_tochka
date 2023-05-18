3157b1bf6d71d885f1f83b25d261457213ef0475
Stanislav <stkubr@gmail.com>
2023-05-18 14:16:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,16 @@ server = Flask(__name__)
 
 app = dash.Dash(__name__, server=server)
 
-credit_data = pd.read_csv('credit_data.csv')
+credit_data = pd.read_csv('credit_data.csv') 
+# columns: client_id, credit_exposure
 client_data = pd.read_csv('client_data.csv')
+# columns: client_id, client_type
 merged_data = pd.merge(credit_data, client_data, on='client_id', how='left')
+
+#############################################################
+# Task 1 
+# add SQl database connection and rewrite lines above to fetch "merged_data" as a SQL single request
+#############################################################
 
 app.layout = html.Div(children=[
     html.H1(children='Credit Exposure by Client Type'),
@@ -31,18 +38,42 @@ def generate_graph(n_clicks):
     fig = px.bar(result, x=result.index, y='credit_exposure')
     return fig
 
+#############################################################
+# Task 2
+# add Flask HTTP endpoint that outputs 10% and 90% quantile credit exposure clients as JSON
+#############################################################
+
+#############################################################
+# Task 3
+# how we can estimate credit risks of such credit portfolio
+#############################################################
+
 if __name__ == '__main__':
     app.run_server(debug=True)
 
     
-# add Flask HTTP endpoint that outputs 10% and 90% quantile credit exposure clients as JSON
-# add SQl database connection and fetch "merged_data" as a single request
-# how we can estimate credit risks of such credit portfolio
-
+    
+############################################################################
 ############################################################################
 
-marks = [2,2,3,5,2,4,4,3,3,5,2,4,3]
+# you are given a list of student's grades ranging from 2 to 5. 
+# Grades can only be natural number from 2 to 5
 
-# calc mean, median, std
-# what kind of distribution this can be?
+marks = [2,2,3,4,2,4,2,4,3,2,3,5,2,4,3,2,2,2,3,3,2,4]
+
+#############################################################
+# Task 4
+# calculate mean, median, std
+#############################################################
+
+#############################################################
+# Task 5
+# what kind of distribution this can be? 
+# what are fat-tail distributions and how can measure the "tail"?
+#############################################################
+
+#############################################################
+# Task 6
 # sort this list and explain O()-difficulty of algrorithm
+#############################################################
+
